be5fdb55f76f72e46d494bdbbfd7202745b6d38b
monicaquintal <monica.zoom@hotmail.com>
2023-01-05 22:11:31 -0300

    criando posições randômicas

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-/* Definindo altura e largura da aplicação */
+/***** Definindo altura e largura da aplicação *****/
 
 var altura = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
-  altura = window.innerHeight
   largura = window.innerWidth
+  altura = window.innerHeight
+
+  console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+/***** Criação de posições randômicas *****/
+
+function posicaoRandomica() {
+
+  var posicaoX = Math.floor(Math.random() * largura) - 90
+  var posicaoY = Math.floor(Math.random() * altura) - 90
+  // decremento de 90px para que a imagem não estoure a dimensão do palco! 
+
+  posicaoX = posicaoX < 0 ? 0 : posicaoX
+  posicaoY = posicaoY < 0 ? 0 : posicaoY
+  // em caso da posição ser < 0, igualará a zero!
+
+  console.log(posicaoX, posicaoY)
+
+  // Criação do elemento HTML
+  var mosquito = document.createElement('img')
+  mosquito.src = './imagens/mosquito.png'
+  mosquito.className = 'mosquito1'
+  mosquito.style.left = posicaoX + 'px'
+  mosquito.style.top = posicaoY + 'px'
+  mosquito.style.position = 'absolute'
+
+  document.body.appendChild(mosquito)
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+<div align="center">
+
+# App Game Mata Mosquito 🦟
+Desenvolvido na Seção 8: JavaScript do Curso Desenvolvimento Web Completo 2022.
+
+</div>
+
+### Aulas 1 e 2: 
+- preparando o ambiente.
+
+### Aula 3:
+- definindo a dimensão do palco do jogo.
+- aplicação window.innerWidth e window.innerHeight.
+
+### Aula 4:
+- criação de posições aleatórias (valores ref. a eixos x e y).
+- aplicação dos métodos Math.random(), Math.floor().
+- manipulação do DOM (createElement, appendChild).
