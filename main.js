398fd57c67725a3653a93e98e00bbef01e662b9d
monicaquintal <monica.zoom@hotmail.com>
2023-01-05 21:32:10 -0300

    definindo a dimensão do palco do jogo

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Definindo altura e largura da aplicação */
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+  altura = window.innerHeight
+  largura = window.innerWidth
+}
+
+ajustaTamanhoPalcoJogo()
